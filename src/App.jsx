38f4d427f8955eea22e1ab3e92828b42109ab4e5
Armaan Mishra <armaanmishra48@gmail.com>
2023-11-16 15:50:16 +0530

    ui updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Results from "./components/Results"
 function App() {
   
   return (
-    <div className="h-screen w-screen bg-stone-950 flex flex-col items-center justify-center antialiased text-white gap-4 p-3">
+    <div className="min-h-screen max-w-screen bg-stone-950 flex flex-col items-center justify-center antialiased text-white gap-4 p-3">
       <h1 className="text-white font-bold text-3xl mb-6">Zip Code Information</h1>
       <div>
         <InputComponent />
